e81886fcdba66df28d9618cb68e95bff6cc20fbe
rafmandrade <rafmandrade@hotmail.com>
2015-01-25 17:07:49 -0500

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Program Assignment 2 ###
 
-## Write a short comment describing this function
-
+###This function creates a special kind of matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL        #initalizing the inverted matrix as a NULL matrix 'inv'
+  
+  set <- function(y){        #method to initialize the matrix to be inverted 'matrix'
+    matrix <<- y             #setting its values with the values of the typed matrix 'y' 
+    y <<- NULL
+  }
+  
+  get <- function(){         #method to return the matrix to be inverted 'matrix'
+    matrix
+  }
+
+  set_inv <- function(inverse){         #method to set the values of the inverted matrix 'ínv' and store it
+    inv <<- inverse                    
+  }
+  
+  get_inv <- function(){                  #method to return the inverted matrix 'inv'
+    inv
+  }
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)     #return the list of methods
 
 }
 
 
-## Write a short comment describing this function
-
+#This functions checks if the matrix was already inverted, then print it if yes; else it inverts the matrix
+#and save it in the upper-environment variable 'inv', then returns the matrix 'ínv'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+  inv <- x$get_inv()           #local variable 'inv' gets the value of the upper-environment variable 'inv'
+  
+  if(!is.null(inv)) {                           #if 'the inverted matrix was already calculated, then 'inv' 
+    message("getting cached data")              #will ve different than NULL. In this case, just print 'inv'.
+    return(inv)
+  }
+
+  matrix <- x$get()                   #local var 'matrix' gets the upper-environment var 'matrix'
+  m <- solve(matrix)                  #m gets the inverse of the local var 'matrix'
+  x$setInverse(m)                     #upper-environment var ínv' gets the local var 'm' 
+  return(x)                           #print the inverted matrix 'm' 
+
 }
